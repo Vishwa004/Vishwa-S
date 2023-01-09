ce4d4bf660d54850a005f01cac53905fda868ee4
Vishwa004 <122216101+Vishwa004@users.noreply.github.com>
2023-01-09 14:04:59 +0530

    Add files via upload

---
+++ eassy5sqcu.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int sq,cu,num;
+	printf("Enter the Integer: ");
+	scanf("%d",&num);
+	sq=num*num;
+	cu=num*num*num;
+	printf("Square Number:%d\n",sq);
+	printf("Cube Number:%d\n",cu);
+}
